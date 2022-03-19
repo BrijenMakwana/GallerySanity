e1f2816b8d530c32588a48f8f7bfff4468199f2e
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-19 11:59:44 +0530

    Schema added

---
+++ schemas/gallery.js
@@ -0,0 +1,12 @@
+export default {
+  name: "gallery",
+  title: "Gallery",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+    },
+  ],
+};
