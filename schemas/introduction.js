256e2f85a9adc285ab8af1fe15b969394beabfa3
Brijen Makwana <brijenmakwana@Brijens-MacBook-Air.local>
2022-03-19 16:35:09 +0530

    Introduction schema added

---
+++ schemas/introduction.js
@@ -0,0 +1,27 @@
+export default {
+  name: "introduction",
+  title: "Introduction",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "about",
+      title: "About",
+      type: "text",
+    },
+    {
+      name: "github",
+      title: "GitHub",
+      type: "url",
+    },
+    {
+      name: "portfolio",
+      title: "Portfolio",
+      type: "url",
+    },
+  ],
+};
